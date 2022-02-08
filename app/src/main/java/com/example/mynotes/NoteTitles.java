a5b2492138d161bf95742fcc840ef366cf2427e3
Vladimir Grigoriev <intvreporting@gmail.com>
2022-02-08 11:21:54 +0200

    Сделаны заготовки: Контейнеры в mainActivity layout Фрагмент с заголовками заметок (NoteTitles) Клас заметок

---
+++ app/src/main/java/com/example/mynotes/NoteTitles.java
@@ -0,0 +1,61 @@
+package com.example.mynotes;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link NoteTitles#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class NoteTitles extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String CURRENT_NOTE = "current_note";
+
+    // TODO: Rename and change types of parameters
+    private MySimpleNote currentNote;
+
+    public NoteTitles() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment NoteTitles.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static NoteTitles newInstance(String param1, String param2) {
+        NoteTitles fragment = new NoteTitles();
+        Bundle args = new Bundle();
+        args.putString(CURRENT_NOTE, param1);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            currentNote = getArguments().getParcelable(CURRENT_NOTE);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note_titles, container, false);
+    }
+}
